622ddd614230b475725643acee89bd5b3a5766da
biwanshrestha14 <biwanshrestha77@gmail.com>
2025-09-05 15:48:27 +0545

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,28 +7,9 @@ export default defineConfig({
   plugins: [react()],
   base: '/portfolio/', // Base path for GitHub Pages
   build: {
-    assetsInlineLimit: 0, // Disable inlining assets
     outDir: 'dist',
     assetsDir: 'assets',
-    copyPublicDir: true, // Ensure public directory files are copied
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const extType = info[info.length - 1]
-          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name)) {
-            return `assets/images/[name]-[hash][extname]`
-          }
-          if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
-            return `assets/fonts/[name]-[hash][extname]`
-          }
-          return `assets/[name]-[hash][extname]`
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
+    copyPublicDir: true,
   },
   resolve: {
     alias: {
